1e84f578da4a633611c616f600db42f83a5d2035
V1ll4a <92698844+V1ll4a@users.noreply.github.com>
2022-02-01 15:14:49 -0500

    Update mario.js

--- mario.js
+++ mario.js
@@ -8,10 +8,9 @@ function piso(nPiso, altura){
    for(let i=0;i<nEspacios; i++){
        texto=texto +"&nbsp;"
    }
-   ;for (let i = 0; i < nNumerales; i++){
-     texto += texto +"#";
+   for (let i = 0; i < nNumerales; i++){
+     texto = texto +"#";
    }
-   {
    texto=texto + "</p>"
    return texto;
    }
